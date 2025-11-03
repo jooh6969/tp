79636267f155826bf7b3a823719e9988f872639a
jooh6969 <xlorddopist@gmail.com>
2025-11-03 17:49:54 +0800

    Fix Checkstyle error

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.file.Files;
