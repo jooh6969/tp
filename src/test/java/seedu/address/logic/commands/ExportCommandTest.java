96415414a323a458d41068f0c7d0af99fd4f873d
jooh6969 <xlorddopist@gmail.com>
2025-11-03 17:53:34 +0800

    Remove unused import

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -7,7 +7,6 @@ import java.nio.file.Path;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 
